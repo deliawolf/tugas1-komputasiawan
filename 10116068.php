f13cb888e15e37c2e6c262b45fa07c142b0b2112
Adit <aditya.maulana.rojak@gmail.com>
2019-10-11 23:54:51 +0700

    commit

---
+++ 10116068.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta class="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Authentication @Evie</title>
+	<link rel='stylesheet' href='css/style.min.css' />
+</head>
+<body>
+	<!-- Authentication pages -->
+	<div class="auth">
+		<div class="container">
+			<div class="auth__inner">
+				<div class="auth__media">
+					<img src="./images/undraw_letter_2k4m.svg">
+				</div>
+				<div class="auth__auth">
+                    <h1 class="hero__title">Aditya Maulana R</h1>
+                    <h1 class="hero__title">10116068</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+<script src='js/app.min.js'></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<meta class="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>Kelompok 6</title>
+	<!-- Don't forget to add your metadata here -->
+	<link rel='stylesheet' href='css/style.css' />
+</head>
+<body>
+	<!-- Hero unit -->
+	<div class="hero">
+		<div class="hero__content">
+		<div class="hero__overlay hero__overlay--gradient"></div>
+		<div class="hero__mask"></div>
+		<div class="page__header__inner">
+			<div class="container">
+					<div class="hero__content__inner" id='navConverter'>
+						<h1 class="hero__title">Kelompok 6</h1>
+						<p class="hero__text">Kelompok 6 berisikan 6 orang pria, dalam pengambilan tema keamanan komputasi awan kelompok ini mengambil pelanggaran data (data breaches)</p>
+						<a href="10116068.php" class="button button__primary">Anggota 1</a>
+						<a href="#" class="button button__primary">Anggota 2</a>
+						<a href="#" class="button button__primary">Anggota 3</a>
+						<a href="#" class="button button__primary">Anggota 4</a>
+						<a href="#" class="button button__primary">Anggota 5</a>
+						<br>
+						<a href="#" class="button button__delete">Review Cloud security</a>
+					</div>
+					</div>
+			</div>
+		</div>
+	</div>
+<script src='js/app.min.js'></script>
+</body>
+</html>
